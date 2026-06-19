8f9fd3cb099b72572e223b26c5ddb5352bf1432e
Francisco Manuel Medina García <medinafran@gmail.com>
2026-06-19 22:08:17 +0100

    proyecto corregido para poder ser descargado

--- Assets/_Scripts/LevelLoader.cs
+++ Assets/_Scripts/LevelLoader.cs
@@ -7,6 +7,14 @@ using UnityEngine.SceneManagement;
 public class LevelLoader : MonoBehaviour
 {
 
+    private void Start()
+    {
+        SceneManager.LoadScene("LevelAmbient", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Mapa", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Victoria", LoadSceneMode.Additive);
+    }
+
+    
 
 
 
